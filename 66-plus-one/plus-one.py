606a2c121206960ef464c5c58558c72989ef6dc3
Anirban Banerjee <144148335+anirbanbanerjee07@users.noreply.github.com>
2025-07-16 23:37:30 +0530

    Time: 0 ms (100.00%) | Memory: 12.3 MB (80.27%) - LeetSync

---
+++ 66-plus-one/plus-one.py
@@ -0,0 +1,9 @@
+class Solution(object):
+    def plusOne(self, digits):
+        for i, d in reversed(list(enumerate(digits))):
+          if d < 9:
+            digits[i] += 1
+            return digits
+          digits[i] = 0
+
+        return [1] + digits
